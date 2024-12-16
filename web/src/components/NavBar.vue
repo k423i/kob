a95b024c5bea95874ed36d6e7bc0cc88713977e0
lyf <1289881056@qq.com>
2024-12-16 15:52:15 +0800

    蛇、延长、键盘输入、终止以及眼睛

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -2,9 +2,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container">
     <router-link class="navbar-brand" :to="{name: 'home'}">King Of Bots</router-link>
-    <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button> -->
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
@@ -24,7 +21,7 @@
           </a>
           <ul class="dropdown-menu">
             <li>
-            <router-link class="dropdown-item" :to="{name: 'user_bot_index'}">我的Bot</router-link>
+              <router-link class="dropdown-item" :to="{name: 'user_bot_index'}">我的Bot</router-link>
             </li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">退出</a></li>
