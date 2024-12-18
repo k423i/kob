24485f0766e264dc47ca4956049ec38a1533cd84
lyf <1289881056@qq.com>
2024-12-18 21:36:32 +0800

    创建个人中心页面

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -4,10 +4,62 @@
 
 <script>
 import ContentField from '../../../components/ContentField.vue'
+import $ from 'jquery'
+import { useStore } from 'vuex'
 
 export default{
     components: {
         ContentField
+    },
+    setup() {
+        const store = useStore();
+        // $.ajax({
+        //     url: "http://localhost:3000/user/bot/add/",
+        //     type: "post",
+        //     data: {
+        //         title: "Bot的标题",
+        //         description: "Bot的描述",
+        //         content: "Bot的代码"
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+        // $.ajax({
+        //     url: "http://localhost:3000/user/bot/remove/",
+        //     type: "post",
+        //     data: {
+        //         bot_id: 80,
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+        $.ajax({
+            url: "http://localhost:3000/user/bot/getlist/",
+            type: "get",
+            headers: {
+                Authorization: "Bearer " + store.state.user.token,
+            },
+            success(resp) {
+                console.log(resp);
+            },
+            error(resp) {
+                console.log(resp);
+            }
+        })
     }
 }
 </script>
